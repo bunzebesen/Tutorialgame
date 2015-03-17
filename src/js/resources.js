3c41a4e7c3c2a991ea112b5104cc504827469fce
Timo <timo_kuester@web.de>
2015-03-17 17:06:09 +0100

    'Initial Commit'

---
+++ src/js/resources.js
@@ -0,0 +1,12 @@
+game.resources = [
+    {
+        "name": "area01",
+        "type": "tmx",
+        "src": "data/map/area01.tmx"
+    },
+    {
+    	"name": "area01_level_tiles",
+    	"type": "image",
+    	"src": "data/img/map/area01_level_tiles.png"
+    }
+];
